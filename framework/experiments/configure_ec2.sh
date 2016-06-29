a919bdf6aea131ea5280d9c2715c093a36db2636
Gregory Gay <greg@greggay.com>
2016-06-29 08:23:53 -0400

    Changes EvoSuite version used on EC2 servers to generate tests

--- framework/experiments/configure_ec2.sh
+++ framework/experiments/configure_ec2.sh
@@ -7,7 +7,7 @@ git clone https://github.com/Greg4cr/defects4j.git
 cd defects4j
 git checkout coverage-experiment
 ./init.sh
-cp framework/lib/test_generation/generation/evosuite-1.0.4.jar framework/lib/test_generation/generation/evosuite-current.jar
-cp framework/lib/test_generation/runtime/evosuite-standalone-runtime-1.0.4.jar framework/lib/test_generation/runtime/evosuite-rt.jar
+cp framework/lib/test_generation/generation/evosuite-1.0.3.jar framework/lib/test_generation/generation/evosuite-current.jar
+cp framework/lib/test_generation/runtime/evosuite-standalone-runtime-1.0.3.jar framework/lib/test_generation/runtime/evosuite-rt.jar
 export PATH=$PATH:/home/ec2-user/defects4j/framework/bin
 echo "export PATH=$PATH:/home/ec2-user/defects4j/framework/bin" >> /home/ec2-user/.bashrc
