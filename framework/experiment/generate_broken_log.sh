133c1884e51963957dee2390ecddb5145819b0bc
Gregory Gay <greg@greggay.com>
2017-05-22 12:24:27 -0400

    scripting to fix broken tests

---
+++ framework/experiment/generate_broken_log.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# Gregory Gay <greg@greggay.com>
+# Parse output of compilation and generate a list of broken classes
+# $1 = output of compilation
+# $2 = directory where original test files are kept 
+# $3 = where to output log file
+# $4 = where to output imports file
+
+cat $1 | gawk -v testDir=$2 -v logFile=$3 -v importFile=$4 'BEGIN{
+		files[0]=0;
+		lines[0]=0;
+		classes[0]=0;	
+		#failingClasses[0]=0;	
+		grabImport=0
+	}
+	/: error:/ {
+		# Parse error messages to get filenames and line numbers
+		split($0,parts,":");
+		split(parts[1],fileNameParts,"]");
+		if(fileNameParts[2] ~ "randoop"){
+			split(fileNameParts[2],fNameParts,"randoop/");
+			files[++files[0]]=testDir"/"fNameParts[2];
+			class=substr(fNameParts[2],0,index(fNameParts[2],".")-1);
+			classes[++classes[0]]=class;
+		}else if(fileNameParts[2] ~ "evosuite"){
+			split(fileNameParts[2],fNameParts,"evosuite");
+			intermediate=substr(fNameParts[2],index(fNameParts[2],"/")+1);
+			files[++files[0]]=testDir"/"intermediate;
+			class=substr(intermediate,0,index(intermediate,".")-1);
+
+			classes[++classes[0]]=class;
+		}
+		lines[++lines[0]]=parts[2];
+		
+		grabImport=1;
+	}
+	/import / {
+		if(grabImport==1){
+			out=classes[classes[0]]",";
+			split($0,parts,"import ");
+			if(parts[2] !~ /junit/){
+				print out parts[2] > importFile;
+			}
+		}
+	}
+	/: warning:/ { grabImport=0;}
+	END{
+		# Find the broken test method
+		for(f=1; f<=files[0]; f++){
+			gsub(/ /,"",files[f]);
+			lNum=0;
+			method="";
+			newTest=0;
+			while(getline x < files[f]){
+				lNum++;
+				if(lNum >= lines[f]){
+					break;
+				}else{
+					#Which test are we looking at?
+					if(x ~ "@Test"){
+						newTest=1;
+					}else if(newTest==1){
+						newTest=0;
+						split(x,parts,"(");
+						split(parts[1],moreParts,"void");
+						method=substr(moreParts[2],2);
+					}
+				}	
+			}	
+			close(files[f]);
+			gsub(/\//,".",classes[f]);
+			if(method==""){
+				#broken=0;
+				#for(c=1;c<=failingClasses[0];c++){
+				#	if(failingClasses[c]==classes[f]){
+				#		broken=1;
+				#		break;
+				#	}
+				#}
+				#if(broken==0){
+					print "--- "classes[f] > logFile;
+				#	failingClasses[++failingClasses[0]]=classes[f];
+				#}
+			}else{
+				#broken=0;
+				#for(c=1;c<=failingClasses[0];c++){
+				#	if(failingClasses[c]==classes[f]){
+				#		broken=1;
+				#		break;
+				#	}
+				#}
+				#if(broken==0){
+					print "--- "classes[f]"::"method > logFile;
+				#}
+			}
+		}
+	}' 
--- framework/experiment/generate_tests_customconfigs.sh
+++ framework/experiment/generate_tests_customconfigs.sh
@@ -59,7 +59,55 @@ for project in $projects; do
 
                                 	# Detect and remove non-compiling tests
 					echo "-----Checking to see if suite needs fixed"
-					perl ../util/fix_test_suite.pl -p $project -d $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial -t $working_dir"/"$project"_"$fault
+					#perl ../util/fix_test_suite.pl -p $project -d $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial -t $working_dir"/"$project"_"$fault
+					continue=1
+					numTestsRemoved=0
+					fixed=0
+					while [ "$continue" -gt "0" ]; do
+						# Run fault detection for each suite to see if it needs fixed
+						mkdir /tmp/tmpResults
+						perl ../bin/run_bug_detection.pl -p $project -d $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial -o $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc -f "**/*Test.java" -t $working_dir"/"$project"_"$fault >> "/tmp/tmpResults/log_"$suite
+						error=`cat "/tmp/tmpResults/log_"$suite | grep error | wc -l`
+				
+						#Do we need to fix it?
+						mkdir /tmp/tmpResults/unpack
+						if [ "$error" -gt "0" ]; then
+							echo "-----Fixing suite"
+							# Unpack the archive
+							tar xvjf $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial"/"$project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" -C "/tmp/tmpResults/unpack"
+							if [ "$fixed" -eq "0" ]; then
+								numTests=`grep -IR @Test /tmp/tmpResults/unpack/ | grep -v ".bak" | wc -l`
+								echo "Num Tests "$numTests
+								let "fixed = 1"
+							fi
+							# Fix the tests		
+							./generate_broken_log.sh "/tmp/tmpResults/log_"$suite "/tmp/tmpResults/unpack/" "/tmp/tmpResults/to_fix_"$project"_"$fault"_"$trial "/tmp/tmpResults/imports_"$project"_"$fault"_"$trial
+							newRemoved=`cat "/tmp/tmpResults/to_fix_"$project"_"$fault"_"$trial | grep "::" | uniq | wc -l`
+							let "numTestsRemoved= $numTestsRemoved + $newRemoved"
+							echo $newRemoved" tests removed, "$numTestsRemoved" to date, out of "$numTests" tests."
+							perl ../util/rm_broken_tests.pl "/tmp/tmpResults/to_fix_"$project"_"$fault"_"$trial "/tmp/tmpResults/unpack/"
+					
+							if [ -a "/tmp/tmpResults/imports_"$project"_"$fault"_"$trial ]; then
+								gawk -f fix_import_statements.awk -v dir="/tmp/tmpResults/unpack/" -v logFile="/tmp/tmpResults/imports_"$project"_"$fault"_"$trial
+							fi
+										
+							# Repack the archive and replace the original				
+							exp_dir=`pwd`
+							cd /tmp/tempResults/unpack
+							tar cvjf $project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" "org/" 
+							mv $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial"/"$project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial"/"$project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2.bak"
+							mv $project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial"/"$project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" 
+							cd $exp_dir
+							rm -rf /tmp/tmpResults/checkout
+							rm -rf /tmp/tmpResults/unpack
+							rm "/tmp/tmpResults/log_"$project"_"$fault"_"$trial
+							rm "/tmp/tmpResults/to_fix_"$project"_"$fault"_"$trial
+							rm "/tmp/tmpResults/imports_"$project"_"$fault"_"$trial
+						else
+							let "continue = 0"
+						fi
+					echo $continue
+					done
 
                         	                # Generate coverage reports
 #                                	        echo "-----Generating coverage reports"
