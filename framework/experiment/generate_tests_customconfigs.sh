222fbe5e973fb347f43d5088d04d0d5eaef716a1
EC2 Default User <ec2-user@ip-172-31-2-255.ec2.internal>
2017-05-22 17:01:47 +0000

    Scripting fix

--- framework/experiment/generate_tests_customconfigs.sh
+++ framework/experiment/generate_tests_customconfigs.sh
@@ -59,55 +59,61 @@ for project in $projects; do
 
                                 	# Detect and remove non-compiling tests
 					echo "-----Checking to see if suite needs fixed"
-					#perl ../util/fix_test_suite.pl -p $project -d $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial -t $working_dir"/"$project"_"$fault
-					continue=1
-					numTestsRemoved=0
-					fixed=0
-					while [ "$continue" -gt "0" ]; do
+					perl ../util/fix_test_suite.pl -p $project -d $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial -t $working_dir"/"$project"_"$fault
+					#continue=1
+					#numTestsRemoved=0
+					#fixed=0
+					#while [ "$continue" -gt "0" ]; do
 						# Run fault detection for each suite to see if it needs fixed
-						mkdir /tmp/tmpResults
-						perl ../bin/run_bug_detection.pl -p $project -d $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial -o $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc -f "**/*Test.java" -t $working_dir"/"$project"_"$fault >> "/tmp/tmpResults/log_"$suite
-						error=`cat "/tmp/tmpResults/log_"$suite | grep error | wc -l`
+					#	mkdir /tmp/tmpResults
+					#	perl ../bin/run_bug_detection.pl -p $project -d $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial -o $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc -f "**/*Test.java" -t $working_dir"/"$project"_"$fault >> "/tmp/tmpResults/log_"$project"_"$fault"_"$trial
+					#	error=`cat "/tmp/tmpResults/log_"$project"_"$fault"_"$trial | grep error | wc -l`
 				
 						#Do we need to fix it?
-						mkdir /tmp/tmpResults/unpack
-						if [ "$error" -gt "0" ]; then
-							echo "-----Fixing suite"
+					#	mkdir /tmp/tmpResults/unpack
+					#	if [ "$error" -gt "0" ]; then
+					#		bugDetection=`cat $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/bug_detection" | grep ","$trial"," | wc -l`
+					#		if [ "$bugDetection" -eq "1" ]; then
+					#			head -n -1 $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/bug_detection" >> tempFile
+					#			mv tempFile $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/bug_detection"
+					#		fi
+
+					#		echo "-----Fixing suite"
 							# Unpack the archive
-							tar xvjf $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial"/"$project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" -C "/tmp/tmpResults/unpack"
-							if [ "$fixed" -eq "0" ]; then
-								numTests=`grep -IR @Test /tmp/tmpResults/unpack/ | grep -v ".bak" | wc -l`
-								echo "Num Tests "$numTests
-								let "fixed = 1"
-							fi
+					#		tar xvjf $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial"/"$project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" -C "/tmp/tmpResults/unpack"
+				#			if [ "$fixed" -eq "0" ]; then
+				#				numTests=`grep -IR @Test /tmp/tmpResults/unpack/ | grep -v ".bak" | wc -l`
+				#				echo "Num Tests "$numTests
+				#				let "fixed = 1"
+				#			fi
 							# Fix the tests		
-							./generate_broken_log.sh "/tmp/tmpResults/log_"$suite "/tmp/tmpResults/unpack/" "/tmp/tmpResults/to_fix_"$project"_"$fault"_"$trial "/tmp/tmpResults/imports_"$project"_"$fault"_"$trial
-							newRemoved=`cat "/tmp/tmpResults/to_fix_"$project"_"$fault"_"$trial | grep "::" | uniq | wc -l`
-							let "numTestsRemoved= $numTestsRemoved + $newRemoved"
-							echo $newRemoved" tests removed, "$numTestsRemoved" to date, out of "$numTests" tests."
-							perl ../util/rm_broken_tests.pl "/tmp/tmpResults/to_fix_"$project"_"$fault"_"$trial "/tmp/tmpResults/unpack/"
+				#			./generate_broken_log.sh "/tmp/tmpResults/log_"$project"_"$fault"_"$trial "/tmp/tmpResults/unpack/" "/tmp/tmpResults/to_fix_"$project"_"$fault"_"$trial "/tmp/tmpResults/imports_"$project"_"$fault"_"$trial
+				#			newRemoved=`cat "/tmp/tmpResults/to_fix_"$project"_"$fault"_"$trial | grep "::" | uniq | wc -l`
+				#			let "numTestsRemoved= $numTestsRemoved + $newRemoved"
+				#			echo $newRemoved" tests removed, "$numTestsRemoved" to date, out of "$numTests" tests."
+				#			perl ../util/rm_broken_tests.pl "/tmp/tmpResults/to_fix_"$project"_"$fault"_"$trial "/tmp/tmpResults/unpack/"
 					
-							if [ -a "/tmp/tmpResults/imports_"$project"_"$fault"_"$trial ]; then
-								gawk -f fix_import_statements.awk -v dir="/tmp/tmpResults/unpack/" -v logFile="/tmp/tmpResults/imports_"$project"_"$fault"_"$trial
-							fi
+				#			if [ -a "/tmp/tmpResults/imports_"$project"_"$fault"_"$trial ]; then
+				#				gawk -f fix_import_statements.awk -v dir="/tmp/tmpResults/unpack/" -v logFile="/tmp/tmpResults/imports_"$project"_"$fault"_"$trial
+				#			fi
 										
 							# Repack the archive and replace the original				
-							exp_dir=`pwd`
-							cd /tmp/tempResults/unpack
-							tar cvjf $project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" "org/" 
-							mv $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial"/"$project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial"/"$project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2.bak"
-							mv $project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial"/"$project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" 
-							cd $exp_dir
-							rm -rf /tmp/tmpResults/checkout
-							rm -rf /tmp/tmpResults/unpack
-							rm "/tmp/tmpResults/log_"$project"_"$fault"_"$trial
-							rm "/tmp/tmpResults/to_fix_"$project"_"$fault"_"$trial
-							rm "/tmp/tmpResults/imports_"$project"_"$fault"_"$trial
-						else
-							let "continue = 0"
-						fi
-					echo $continue
-					done
+				#			exp_dir=`pwd`
+				#			cd /tmp/tmpResults/unpack
+				#			tar cvjf $project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" "org/" 
+				#			mv $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial"/"$project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial"/"$project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2.bak"
+				#			mv $project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" $result_dir"/suites/"$project"_"$fault"/"$budget"/"$project"/evosuite-"$crinosc"/"$trial"/"$project"-"$fault"f-evosuite-"$crinosc"."$trial".tar.bz2" 
+				#			cd $exp_dir
+				#			rm -rf /tmp/tmpResults/checkout
+				#			rm -rf /tmp/tmpResults/unpack
+				#			rm "/tmp/tmpResults/log_"$project"_"$fault"_"$trial
+				#			rm "/tmp/tmpResults/to_fix_"$project"_"$fault"_"$trial
+				#			rm "/tmp/tmpResults/imports_"$project"_"$fault"_"$trial
+				#		else
+				#			let "continue = 0"
+				#		fi
+				#	echo $continue
+				#	done
 
                         	                # Generate coverage reports
 #                                	        echo "-----Generating coverage reports"
@@ -136,7 +142,7 @@ for project in $projects; do
 			done
 		done
 		# Back up to cloud
-		tar cvzf $project"_"$fault"_"$budgets"_custom.tgz" $result_dir"/suites/"$project"_"$fault"/"
+		tar cvzf $project"_"$fault"_"$budgets".tgz" $result_dir"/suites/"$project"_"$fault"/"
 	#	scp $project"_"$fault"_"$budgets"_custom.tgz" bstech@blankslatetech.com:/home/bstech/greggay.com/data/	
 	done
 done
